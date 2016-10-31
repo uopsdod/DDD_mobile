66cf2e4fef54e454ee5da9e8931fc61071da30fa
uopsdod <uopsdod@gmail.com>
2016-10-31 14:36:06 +0800

    修改 共住介面增加 使用gson與server拿取資料並顯示

--- app/src/main/java/com/example/sam/drawerlayoutprac/Common.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Common.java
@@ -5,7 +5,7 @@ package com.example.sam.drawerlayoutprac;
  */
 
 public class Common {
-    // Android官方模擬器連結本機web server可以直接使用 http://10.0.2.2
+    //   Android官方模擬器連結本機web server可以直接使用 http://10.0.2.2
     //public static String URL = "http://10.0.2.2:8081/TextToJson_Web/SearchServlet";
     public final static String URL = "http://10.0.2.2:8081/TextToJson_Web/Partner";
 }
