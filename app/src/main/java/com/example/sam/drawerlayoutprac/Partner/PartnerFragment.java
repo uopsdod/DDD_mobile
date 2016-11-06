d52fdec2d7b33bdff18545e26af571bdb941e855
uopsdod <uopsdod@gmail.com>
2016-11-06 15:06:17 +0800

    調整共住版面: 新增返回按鈕功能與動畫

--- app/src/main/java/com/example/sam/drawerlayoutprac/Partner/PartnerFragment.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Partner/PartnerFragment.java
@@ -7,12 +7,14 @@ import android.app.ProgressDialog;
 import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RoundRectShape;
+import android.icu.text.MessagePattern;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,10 +33,13 @@ import android.widget.TextView;
 import com.example.sam.drawerlayoutprac.Common;
 import com.example.sam.drawerlayoutprac.R;
 import com.example.sam.drawerlayoutprac.Util;
+import com.nhaarman.listviewanimations.appearance.ViewAnimator;
+import com.nhaarman.listviewanimations.appearance.simple.SwingLeftInAnimationAdapter;
 import com.yalantis.euclid.library.EuclidListAdapter;
 import com.yalantis.euclid.library.EuclidState;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -54,7 +59,6 @@ public class PartnerFragment extends Fragment {
     ListView listview;
 
     //testing
-    private EuclidState mState = EuclidState.Closed;
     private static final int MAX_DELAY_SHOW_DETAILS_ANIMATION = 500;
     private static final int CIRCLE_RADIUS_DP = 50;
     private static final int REVEAL_ANIMATION_DURATION = 1000;
@@ -72,6 +76,19 @@ public class PartnerFragment extends Fragment {
     private AnimatorSet mOpenProfileAnimatorSet;
     protected LinearLayout mProfileDetails;
 
+    // testing
+    private EuclidState mState = EuclidState.Closed;
+    private AnimatorSet mCloseProfileAnimatorSet;
+    private static final int STEP_DELAY_HIDE_DETAILS_ANIMATION = 80;
+    private static final int ANIMATION_DURATION_CLOSE_PROFILE_DETAILS = 500;
+    private SwingLeftInAnimationAdapter mListViewAnimationAdapter;
+    private ViewAnimator mListViewAnimator;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        backBtnPressed();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
@@ -92,6 +109,74 @@ public class PartnerFragment extends Fragment {
                 mInitialProfileButtonX = mButtonProfile.getX();
             }
         });
+
+
+
+        // get data
+        // 檢查使用者是否有連線功能
+//        if (Common.networkConnected(getActivity())) {
+//            // send request to server and get the response - 重點在new這個動作
+//            String url = Common.URL + "/live2/Partner";
+//            Log.d("url", url);
+//            // 注意:listview的setAdapter是在PartnerGetTextTask裡面設定的
+//            retrievePartnerTask = new PartnerGetTextTask(getContext(), this.listview).execute(url);
+//        } else {
+//            Util.showToast(getActivity(), "no network");
+//        }
+        // end of get data
+
+        // set up floatingBtn click Listener
+        LinearLayout ll_view = (LinearLayout) viewGroup.getParent();
+        CoordinatorLayout cdl_view = (CoordinatorLayout) ll_view.getParent();
+        FloatingActionButton floatingBtn = (FloatingActionButton) cdl_view.findViewById(R.id.floatingBtn);
+
+        floatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Util.showToast(getContext(), "ftBtn clicked");
+                Fragment fragment = new PartnerMapFragment();
+                Util.switchFragment(PartnerFragment.this, fragment);
+            }
+        });
+        try {
+            initList();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }// end of onCreateView
+
+    private void initList() throws ExecutionException, InterruptedException {
+        String url = Common.URL + "/live2/Partner";
+        List<MemVO> memVOList = (List<MemVO>)new PartnerGetTextTask(getContext(), this.listview).execute(url).get();
+        Map<String, Object> profileMap;
+        List<Map<String, Object>> profilesList = new ArrayList<>();
+
+        for (int i = 0; i < memVOList.size(); i++) {
+            MemVO myVO = memVOList.get(i);
+
+            profileMap = new HashMap<>();
+
+            //放入文字資料
+            profileMap.put(PartnerListAdapter.KEY_MEMID, myVO.getMemId());
+            profileMap.put(PartnerListAdapter.KEY_NAME, myVO.getMemName());
+            profileMap.put(PartnerListAdapter.KEY_DESCRIPTION_SHORT, myVO.getMemIntro());
+            profileMap.put(PartnerListAdapter.KEY_DESCRIPTION_FULL, myVO.getMemIntro());
+            profilesList.add(profileMap);
+        }
+
+        mListViewAnimationAdapter = new SwingLeftInAnimationAdapter(new PartnerListAdapter(getContext(), R.layout.list_item, profilesList));
+        mListViewAnimationAdapter.setAbsListView(this.listview);
+        mListViewAnimator = mListViewAnimationAdapter.getViewAnimator();
+        if (mListViewAnimator != null) {
+            mListViewAnimator.setAnimationDurationMillis(getAnimationDurationCloseProfileDetails());
+            mListViewAnimator.disableAnimations();
+        }
+
+        PartnerFragment.this.listview.setAdapter(mListViewAnimationAdapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -108,35 +193,6 @@ public class PartnerFragment extends Fragment {
                 }
             }
         });
-
-
-        // get data
-        // 檢查使用者是否有連線功能
-        if (Common.networkConnected(getActivity())) {
-            // send request to server and get the response - 重點在new這個動作
-            String url = Common.URL + "/live2/Partner";
-            Log.d("url", url);
-            retrievePartnerTask = new PartnerGetTextTask(getContext(), this.listview).execute(url);
-        } else {
-            Util.showToast(getActivity(), "no network");
-        }
-        // end of get data
-
-        // set up floatingBtn click Listener
-        LinearLayout ll_view = (LinearLayout) viewGroup.getParent();
-        CoordinatorLayout cdl_view = (CoordinatorLayout) ll_view.getParent();
-        FloatingActionButton floatingBtn = (FloatingActionButton) cdl_view.findViewById(R.id.floatingBtn);
-
-        floatingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Util.showToast(getContext(), "ftBtn clicked");
-                Fragment fragment = new PartnerMapFragment();
-                Util.switchFragment(PartnerFragment.this, fragment);
-            }
-        });
-
-        return view;
     }
 
     private void showProfileDetails(Map<String, Object> item, final View view) throws ExecutionException, InterruptedException {
@@ -369,4 +425,113 @@ public class PartnerFragment extends Fragment {
         mTextViewProfileName.setText((String) item.get(EuclidListAdapter.KEY_NAME));
         mTextViewProfileDescription.setText((String) item.get(EuclidListAdapter.KEY_DESCRIPTION_FULL));
     }
-}
+
+
+
+    private void backBtnPressed() {
+        getView().setFocusableInTouchMode(true);
+        getView().requestFocus();
+        getView().setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
+                    Util.showToast(getContext(),"backBtnPressed");
+//                    mWrapper.removeView(mOverlayListItemView);
+//                    mToolbarProfile.setVisibility(View.INVISIBLE);
+//                    mProfileDetails.setVisibility(View.INVISIBLE);
+//                    PartnerFragment.this.listview.bringToFront();
+//                    return true;
+
+                    if (getState() == EuclidState.Opened) {
+                        animateCloseProfileDetails();
+                    } else if (getState() == EuclidState.Closed) {
+                        //this.onKey(v, keyCode, event);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public EuclidState getState() {
+        return mState;
+    }
+
+    private void animateCloseProfileDetails() {
+        mState = EuclidState.Closing;
+        getCloseProfileAnimatorSet().start();
+    }
+
+    private AnimatorSet getCloseProfileAnimatorSet() {
+        if (mCloseProfileAnimatorSet == null) {
+            Animator profileToolbarAnimator = ObjectAnimator.ofFloat(mToolbarProfile, View.X,
+                    0, mToolbarProfile.getWidth());
+
+            Animator profilePhotoAnimator = ObjectAnimator.ofFloat(mOverlayListItemView, View.X,
+                    0, mOverlayListItemView.getWidth());
+            profilePhotoAnimator.setStartDelay(getStepDelayHideDetailsAnimation());
+
+            Animator profileButtonAnimator = ObjectAnimator.ofFloat(mButtonProfile, View.X,
+                    mInitialProfileButtonX, mOverlayListItemView.getWidth() + mInitialProfileButtonX);
+            profileButtonAnimator.setStartDelay(getStepDelayHideDetailsAnimation() * 2);
+
+            Animator profileDetailsAnimator = ObjectAnimator.ofFloat(mProfileDetails, View.X,
+                    0, mToolbarProfile.getWidth());
+            profileDetailsAnimator.setStartDelay(getStepDelayHideDetailsAnimation() * 2);
+
+            List<Animator> profileAnimators = new ArrayList<>();
+            profileAnimators.add(profileToolbarAnimator);
+            profileAnimators.add(profilePhotoAnimator);
+            profileAnimators.add(profileButtonAnimator);
+            profileAnimators.add(profileDetailsAnimator);
+
+            mCloseProfileAnimatorSet = new AnimatorSet();
+            mCloseProfileAnimatorSet.playTogether(profileAnimators);
+            mCloseProfileAnimatorSet.setDuration(getAnimationDurationCloseProfileDetails());
+            mCloseProfileAnimatorSet.setInterpolator(new AccelerateInterpolator());
+            mCloseProfileAnimatorSet.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    if (mListViewAnimator != null) {
+                        mListViewAnimator.reset();
+                        mListViewAnimationAdapter.notifyDataSetChanged();
+                    }
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mToolbarProfile.setVisibility(View.INVISIBLE);
+                    mButtonProfile.setVisibility(View.INVISIBLE);
+                    mProfileDetails.setVisibility(View.INVISIBLE);
+
+                    PartnerFragment.this.listview.setEnabled(true);
+                    mListViewAnimator.disableAnimations();
+
+                    mState = EuclidState.Closed;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
+        return mCloseProfileAnimatorSet;
+    }
+
+    protected int getStepDelayHideDetailsAnimation() {
+        return STEP_DELAY_HIDE_DETAILS_ANIMATION;
+    }
+
+    protected int getAnimationDurationCloseProfileDetails() {
+        return ANIMATION_DURATION_CLOSE_PROFILE_DETAILS;
+    }
+
+
+}// end
