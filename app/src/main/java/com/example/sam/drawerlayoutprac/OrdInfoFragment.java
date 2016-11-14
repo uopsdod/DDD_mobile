332163a3f753939480b49e86535a5681ced662de
plzdongo <x123652222@gmail.com>
2016-11-14 12:03:51 +0800

    新增 ordInfo layout 跟 OrdInfo.java檔(訂房須知) 並且抓到浮動的房間價格

---
+++ app/src/main/java/com/example/sam/drawerlayoutprac/OrdInfoFragment.java
@@ -0,0 +1,27 @@
+package com.example.sam.drawerlayoutprac;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by cuser on 2016/11/13.
+ */
+
+public class OrdInfoFragment extends Fragment{
+    TextView tvInfo;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View myLayout = inflater.inflate(R.layout.fragment_ordinfo, container, false);
+        getActivity().findViewById(R.id.floatingBtn).setVisibility(View.INVISIBLE);
+
+        return myLayout;
+    }
+}
