64589d51c22e5706066465c828143d1fa6555830
plzdongo <x123652222@gmail.com>
2016-11-15 15:20:03 +0800

    旅館詳情已可以取到浮動的房間價錢了 稍微做一些版面修改  有上架房間才會出現  房間詳情的星星拿掉了

--- app/src/main/java/com/example/sam/drawerlayoutprac/Room/RoomFragment.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Room/RoomFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.sam.drawerlayoutprac.Common;
@@ -28,7 +29,7 @@ import java.util.concurrent.ExecutionException;
 public class RoomFragment extends Fragment {
     private String TAG = "RoomFragment";
     private TextView tvRoomName,tvFacilitiesDetail, tvPrice;
-    private String RoomId ;
+    private String RoomId;
     private ImageView imageView;
     private RecyclerView rv_RoomImage;
     @Nullable
