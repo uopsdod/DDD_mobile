4a88b5df3bebb06a44a660c679cc47fc61082800
plzdongo <x123652222@gmail.com>
2016-11-17 14:44:00 +0800

    增加會員資料顯示頁面並把登出家在裡面 navigation menu登出跟會員資料移至會員資料內

---
+++ app/src/main/java/com/example/sam/drawerlayoutprac/MemberInfoFragment.java
@@ -0,0 +1,103 @@
+package com.example.sam.drawerlayoutprac;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sam.drawerlayoutprac.Hotel.HotelFragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Created by cuser on 2016/11/17.
+ */
+
+public class MemberInfoFragment extends Fragment {
+    TextView memName, memGender, memLiveBudget, memIntro;
+    ImageView ivPhoto;
+    Button btSubmit, btLogout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_member_info, container, false);
+        memName = (TextView) view.findViewById(R.id.memName);
+        memGender = (TextView) view.findViewById(R.id.memGender);
+        memLiveBudget = (TextView) view.findViewById(R.id.memLiveBudget);
+        memIntro = (TextView) view.findViewById(R.id.memIntro);
+        ivPhoto = (ImageView) view.findViewById(R.id.ivPhoto);
+        btSubmit = (Button) view.findViewById(R.id.btSubmit);
+        btLogout = (Button) view.findViewById(R.id.btLogout);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new MemberUpdateFragment();
+                Util.switchFragment(getActivity(), fragment);
+            }
+        });
+
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //會員登出
+                SharedPreferences pref2 = getActivity().getSharedPreferences(Common.PREF_FILE,
+                        MODE_PRIVATE);
+                boolean login2 = pref2.getBoolean("login",false);
+                if(login2){
+                    pref2.edit().remove("userName")
+                            .remove("password")
+                            .remove("memId")
+                            .remove("login").apply();
+                    Fragment fragment = new HotelFragment();
+                    Util.switchFragment(MemberInfoFragment.this, fragment);
+                    Util.showToast(getContext(),"登出了喔! 屁孩");
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showMemInfo();
+    }
+
+    private void showMemInfo(){
+        SharedPreferences pref = getActivity().getSharedPreferences(Common.PREF_FILE,
+                MODE_PRIVATE);
+        String  id = pref.getString("memId", null);
+        Log.d("-----****--", "id" + id);
+        if(id != null){
+            if(Common.networkConnected(getActivity())){
+                String url = Common.URL + "/android/mem.do";
+                MemVO memVO = null;
+                try{
+                   memVO = new MemGetOneTask().execute(url, id).get();
+                }catch(Exception e){
+                    Log.e("MemberInfo", e.toString());
+                }
+
+                memName.setText(memVO.getMemName());
+                memLiveBudget.setText(memVO.getMemLiveBudget().toString());
+                memIntro.setText(memVO.getMemIntro());
+                if(memVO.getMemGender().equals("F") || memVO.getMemGender().equals("f")){
+                    memGender.setText("女");
+                }else{
+                    memGender.setText("男");
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/sam/drawerlayoutprac/Room/RoomFragment.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Room/RoomFragment.java
@@ -3,7 +3,6 @@ package com.example.sam.drawerlayoutprac.Room;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.icu.util.BuddhistCalendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,12 +13,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.sam.drawerlayoutprac.Common;
-import com.example.sam.drawerlayoutprac.Hotel.HotelGetOneTask;
-import com.example.sam.drawerlayoutprac.Hotel.HotelVO;
 import com.example.sam.drawerlayoutprac.R;
 import com.example.sam.drawerlayoutprac.Util;
 
@@ -74,7 +70,7 @@ public class RoomFragment extends Fragment {
             List<String> roomVOList = null;
             int imageSize = 250;
             try{
-                roomVOList = new RoomGetOneTask2().execute(url, id, imageSize).get();
+                roomVOList = new RoomGetOneAllPhotoTask().execute(url, id, imageSize).get();
             }catch(Exception e){
                 Log.e(TAG,e.toString());
             }
