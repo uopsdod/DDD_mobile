d9e2fc1b214149af07eb49b1d2c87da28fd9d158
uopsdod <uopsdod@gmail.com>
2016-11-20 12:55:16 +0800

    將連線條回本地localhost版本

--- app/src/main/java/com/example/sam/drawerlayoutprac/Common.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Common.java
@@ -12,16 +12,16 @@ import android.net.NetworkInfo;
 public class Common {
     //Android官方模擬器連結本機web server可以直接使用 http://10.0.2.2
     //localhost版本:
-//    public final static String URL = "http://10.0.2.2:8081/DDD_web";
-//    public static String URL_Partner = Common.URL + "/android/live2/partner.do";
-//    public static String URL_PartnerMsgController = Common.URL + "/android/live2/PartnerMsgController";
-//    public static final String URL_Chatroom = "ws://10.0.2.2:8081/DDD_web/android/live2/MsgCenter";
-
-    // Amazon AWS版本:
-    public final static String URL = "http://ec2-54-186-77-188.us-west-2.compute.amazonaws.com:8081/DDD_web";
+    public final static String URL = "http://10.0.2.2:8081/DDD_web";
     public static String URL_Partner = Common.URL + "/android/live2/partner.do";
     public static String URL_PartnerMsgController = Common.URL + "/android/live2/PartnerMsgController";
-    public static final String URL_Chatroom = "ws://ec2-54-186-77-188.us-west-2.compute.amazonaws.com:8081/DDD_web/android/live2/MsgCenter";
+    public static final String URL_Chatroom = "ws://10.0.2.2:8081/DDD_web/android/live2/MsgCenter";
+
+    // Amazon AWS版本:
+//    public final static String URL = "http://ec2-54-186-77-188.us-west-2.compute.amazonaws.com:8081/DDD_web";
+//    public static String URL_Partner = Common.URL + "/android/live2/partner.do";
+//    public static String URL_PartnerMsgController = Common.URL + "/android/live2/PartnerMsgController";
+//    public static final String URL_Chatroom = "ws://ec2-54-186-77-188.us-west-2.compute.amazonaws.com:8081/DDD_web/android/live2/MsgCenter";
 
 
     public final static String PREF_FILE = "preference";
