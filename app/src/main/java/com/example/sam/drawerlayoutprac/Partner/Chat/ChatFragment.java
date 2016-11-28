185ef63dce9548bdb3eeabc9d32b5e5d59735ba9
uopsdod <uopsdod@gmail.com>
2016-11-28 16:21:49 +0800

    穩定共住聊天，增加錯誤處理 若與server連線因server重啟而中斷，幫使用者偷偷的再次連線並寄送訊息

--- app/src/main/java/com/example/sam/drawerlayoutprac/Partner/Chat/ChatFragment.java
+++ app/src/main/java/com/example/sam/drawerlayoutprac/Partner/Chat/ChatFragment.java
@@ -18,6 +18,7 @@ import com.example.sam.drawerlayoutprac.Common;
 import com.example.sam.drawerlayoutprac.MainActivity;
 import com.example.sam.drawerlayoutprac.CommonFragment;
 import com.example.sam.drawerlayoutprac.Partner.PartnerFragment;
+import com.example.sam.drawerlayoutprac.Partner.TokenIdWebSocket;
 import com.example.sam.drawerlayoutprac.Partner.VO.MemVO;
 import com.example.sam.drawerlayoutprac.Partner.PartnerGetOneImageTask;
 import com.example.sam.drawerlayoutprac.Partner.PartnerGetOneTextTask;
@@ -30,6 +31,7 @@ import com.google.gson.GsonBuilder;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.exceptions.WebsocketNotConnectedException;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
@@ -100,7 +102,7 @@ public class ChatFragment extends CommonFragment {
             @Override
             public void onClick(View v) {
                 String newMsg = ChatFragment.this.msg.getText().toString();
-                Util.showToast(getContext(), ChatFragment.TAG + ChatFragment.this.msg.getText().toString());
+                //Util.showToast(getContext(), ChatFragment.TAG + ChatFragment.this.msg.getText().toString());
                 if (newMsg.trim().isEmpty()) {
                     Util.showToast(getContext(), "message is empty.");
                     return;
@@ -113,18 +115,29 @@ public class ChatFragment extends CommonFragment {
                 partnerMsg.setMemChatToMemId(ChatFragment.this.toMemId);
                 partnerMsg.setMemChatContent(newMsg);
                 partnerMsg.setMemChatDate(new Timestamp(new java.util.Date().getTime()));
+                partnerMsg.setFromMobile("fromMobile");
                 // 在自己頁面顯示聊天視窗:
                 addMsgScrollDown(partnerMsg);
                 // end of 在自己頁面顯示聊天視窗
 
                 if (myWebSocketClient != null) {
+                    Log.d(TAG, "myWebSocketClient.getConnection() - " + myWebSocketClient.getConnection());
 //                    Gson gson = new Gson();
                     Gson gson = new GsonBuilder()
                             .setDateFormat("yyyy-MM-dd hh:mm:ss.S")
                             .create(); // 注意:如果VO中有Date,Timestamp，就要Server,Client端規格一致
                     String partnerMsgGson = gson.toJson(partnerMsg);
-                    myWebSocketClient.send(partnerMsgGson);
-                    Log.d("ChatFragment","fcm - " + partnerMsgGson);
+                    try {
+                        myWebSocketClient.send(partnerMsgGson);
+                        Log.d(ChatFragment.TAG, "" + partnerMsgGson);
+                    } catch (WebsocketNotConnectedException e) {
+                        // 如果websocket連線已中斷，幫使用者偷偷的再重新連線一次，並將訊息再次寄出 1
+                        Util.showToast(getContext(), "Not connected to Server. Please relogin.");
+                        new TokenIdWebSocket(getContext()).sendTokenIdToServer();
+                            // 若在bindMemIdWithSession(partnerMsg);有放入partnerMsg物件，則代表這次的binding要加上resend的動作 2
+                        myWebSocketClient = new PartnerChatWebSocket().bindMemIdWithSession(partnerMsg);
+                    }
+
                 }
             }
         });
@@ -136,26 +149,26 @@ public class ChatFragment extends CommonFragment {
         // 拿toMemId的會員Id
         Bundle myBundle = getArguments();
         this.toMemId = (String) myBundle.get("ToMemId");
-        Util.showToast(getContext(), "toMemId: " + this.toMemId);
+        //Util.showToast(getContext(), "toMemId: " + this.toMemId);
         // 拿memId自己的會員Id
-        SharedPreferences preferences_r = getActivity().getSharedPreferences(Common.PREF_FILE,Context.MODE_PRIVATE);
-        String memId = preferences_r.getString("memId",null);
+        SharedPreferences preferences_r = getActivity().getSharedPreferences(Common.PREF_FILE, Context.MODE_PRIVATE);
+        String memId = preferences_r.getString("memId", null);
         // 拿memId自己的大頭照
         String url = Common.URL_Partner;
         int imageSize = 150;
         Bitmap bitmap_memId = getProfileBigmap(url, memId, imageSize);
 
-        profileMap.put(memId,bitmap_memId);
+        profileMap.put(memId, bitmap_memId);
         // 拿memId自己的姓名:
         MemVO memVO_memId = getMemVO(url, memId);
-        this.nameMap.put(memId,memVO_memId.getMemName());
+        this.nameMap.put(memId, memVO_memId.getMemName());
 
         // 拿toMemId的會員大頭照
         Bitmap bitmap_toMemId = getProfileBigmap(url, this.toMemId, imageSize);
-        profileMap.put(this.toMemId,bitmap_toMemId);
+        profileMap.put(this.toMemId, bitmap_toMemId);
         // 拿toMemId的會員的姓名:
-        MemVO memVO_toMemId = getMemVO(url,this.toMemId);
-        this.nameMap.put(this.toMemId,memVO_toMemId.getMemName());
+        MemVO memVO_toMemId = getMemVO(url, this.toMemId);
+        this.nameMap.put(this.toMemId, memVO_toMemId.getMemName());
     }
 
     private void initMsgHistoryList() {
@@ -169,14 +182,14 @@ public class ChatFragment extends CommonFragment {
             e.printStackTrace();
         }
         if (this.partnerMsgList != null && this.partnerMsgList.size() > 0) {
-            Log.d("ChatFragment", "fcm - " + this.partnerMsgList.get(0).getMemChatContent());
+            Log.d(ChatFragment.TAG, "" + this.partnerMsgList.get(0).getMemChatContent());
         }
         this.partnerChatAdapter = new PartnerChatListAdapter(getContext(), this.partnerMsgList);
         this.chatContent.setAdapter(partnerChatAdapter);
         // scroll to the bottom:
-        this.chatContent.post(new Runnable(){
+        this.chatContent.post(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 ChatFragment.this.chatContent.setSelection(ChatFragment.this.partnerChatAdapter.getCount() - 1);
             }
         });
@@ -186,16 +199,20 @@ public class ChatFragment extends CommonFragment {
     public void onPause() {
         super.onPause();
         ChatFragment.this.myWebSocketClient.close();
-        Log.d(ChatFragment.TAG, " fcm - myWebSocketClient is closed via onPause()");
+        Log.d(ChatFragment.TAG, "myWebSocketClient is closed via onPause()");
     }
 
     private class PartnerChatWebSocket {
         URI uri;
 
         public PartnerChatWebSocket() {
+
+        }
+        public WebSocketClient bindMemIdWithSession(){
+            return bindMemIdWithSession(null);
         }
 
-        public WebSocketClient bindMemIdWithSession() {
+        public WebSocketClient bindMemIdWithSession(PartnerMsg aPartnerMsgResend) {
             WebSocketClient tmpWebSocketClient = null;
             SharedPreferences preferences_r = getActivity().getSharedPreferences(Common.PREF_FILE, getActivity().MODE_PRIVATE);
             String memId = null;
@@ -214,7 +231,8 @@ public class ChatFragment extends CommonFragment {
                 partnerMsg.setAction("bindMemIdWithSession");
                 partnerMsg.setMemChatMemId(memId);
                 partnerMsg.setMemChatToMemId(ChatFragment.this.toMemId);
-                tmpWebSocketClient = new PartnerChatWebSocket.MyWebSocketClient(partnerMsg);
+                // 將要resend的物件放入建構式中 3
+                tmpWebSocketClient = new PartnerChatWebSocket.MyWebSocketClient(partnerMsg, aPartnerMsgResend);
                 tmpWebSocketClient.connect();
             }
             return tmpWebSocketClient;
@@ -223,22 +241,33 @@ public class ChatFragment extends CommonFragment {
 
         private class MyWebSocketClient extends WebSocketClient {
             PartnerMsg partnerMsg;
+            PartnerMsg partnerMsgResend;
 
-            public MyWebSocketClient(PartnerMsg aPartnerMsg) {
+            public MyWebSocketClient(PartnerMsg aPartnerMsg, PartnerMsg aPartnerMsgResend) {
                 super(PartnerChatWebSocket.this.uri, new Draft_17());
                 this.partnerMsg = aPartnerMsg;
+                // 將要resend的物件用實體變數接起來 4
+                this.partnerMsgResend = aPartnerMsgResend;
             }
 
             @Override
             public void onOpen(ServerHandshake handshakedata) {
                 Gson gson = new Gson();
                 // 再判斷看要不要用
-//            Gson gson = new GsonBuilder()
+//             Gson gson = new GsonBuilder()
 //                    .setDateFormat("yyyy-MM-dd hh:mm:ss.S")
 //                    .create();
+
+                // 在此判斷如是否有要resend的物件，如果有則在要透過bindMemIdWithSession中加入resend字串 5
+                // Server端會收到有著resend字串的Message，透過bindMemIdWithSession中判斷後，如果有則會再將message寄回來 6
+                if (MyWebSocketClient.this.partnerMsgResend != null) {
+                    this.partnerMsg.setResend("resend");
+                }
                 String partnerMsgGson = gson.toJson(this.partnerMsg);
                 this.send(partnerMsgGson);
-                Log.d("TokenIdWebSocket - ", "fcm - sent to Server(" + this.partnerMsg.getAction() + "): " + partnerMsgGson);
+                Log.d(ChatFragment.TAG, "" + this.partnerMsg.getAction() + "): " + partnerMsgGson);
+
+
             }
 
             @Override
@@ -249,8 +278,19 @@ public class ChatFragment extends CommonFragment {
                         .setDateFormat("yyyy-MM-dd hh:mm:ss.S")
                         .create();
                 // end of 處理Oracle Timestamp型態與gson之間的格式問題
-                Log.d(TAG, "fcm - " + message);
+                Log.d(TAG, "" + message);
                 final PartnerMsg partnerMsg = gson.fromJson(message, PartnerMsg.class);
+
+                // 如果是重新連線要重送的話:
+                // 判斷傳回來的的Message是否有resend字串，如果有則進行重送 7
+                if (partnerMsg.getResend() != null) {
+                    String partnerMsgResendGson = gson.toJson(MyWebSocketClient.this.partnerMsgResend);
+                    MyWebSocketClient.this.send(partnerMsgResendGson);
+                    Log.d(TAG,"resend msg " + partnerMsgResendGson);
+                    return;
+                }
+
+
                 // 將data放入partnerMsgList,並更新UI畫面
                 // Try - 使用AsynTask加快訊息的接收
                 Thread myThread = new Thread(new Runnable() {
@@ -279,7 +319,7 @@ public class ChatFragment extends CommonFragment {
 
             @Override
             public void onClose(int code, String reason, boolean remote) {
-
+                Log.d(ChatFragment.TAG, " myWebSocketClient is closed via onClose()");
             }
 
             @Override
@@ -291,19 +331,19 @@ public class ChatFragment extends CommonFragment {
 
     }// end of PartnerChatWebSocket
 
-    private void addMsgScrollDown(PartnerMsg partnerMsg){
+    private void addMsgScrollDown(PartnerMsg partnerMsg) {
         ChatFragment.this.partnerMsgList.add(partnerMsg);
         ChatFragment.this.partnerChatAdapter.notifyDataSetChanged();
         // scroll down to the bottom:
-        ChatFragment.this.chatContent.post(new Runnable(){
+        ChatFragment.this.chatContent.post(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 ChatFragment.this.chatContent.setSelection(ChatFragment.this.partnerChatAdapter.getCount() - 1);
             }
         });
     }
 
-    private Bitmap getProfileBigmap(String aUrl, String aMemId, Integer aImageSize){
+    private Bitmap getProfileBigmap(String aUrl, String aMemId, Integer aImageSize) {
         Bitmap bitmap_memId = null;
         try {
             bitmap_memId = new PartnerGetOneImageTask(null).execute(aUrl, aMemId, aImageSize).get();
@@ -315,7 +355,7 @@ public class ChatFragment extends CommonFragment {
         return bitmap_memId;
     }
 
-    private MemVO getMemVO(String aUrl, String aMemId){
+    private MemVO getMemVO(String aUrl, String aMemId) {
         MemVO memVO_memId = null;
         try {
             memVO_memId = new PartnerGetOneTextTask().execute(aUrl, aMemId).get();
@@ -349,3 +389,27 @@ public class ChatFragment extends CommonFragment {
         });
     }// end of backBtnPressed
 }
+
+
+/* 測試驗收流程
+
+1. 會員01登入:
+* 檢查是否上傳token
+2. 會員01向會員02傳訊息：
+* 檢查是否建立session
+3. 會員01保持登入，將server重啟，會員01再向會員02傳訊息：
+* 檢查是否跑resend流程
+* 檢查是否再次上傳token
+* 檢查是否再次建立session
+4. 會員01保持登入，會員02也登入:
+* 檢查前面的訊息是否皆能再會員02的歷史訊息中看到
+5. 會員01保持登入，會員02也登入，但會員02不在聊天頁面，會員01向會員02傳訊息:
+* 檢查會員02是否收到FCM notification
+* 檢查會員02點擊notification後是否跳轉到聊天頁面
+6. 會員01保持登入，會員02也登入，且兩者都在同一個聊天頁面，會員01與會員02互相傳訊息:
+* 檢查會員02與會員01是否能即時收到訊息
+7. 會員01保持登入，會員02也保持登入，但改成會員01向會員03傳訊息:
+* 檢查是否會員02"不會"收到notification
+* 檢查是否會員02也"不會"收到即時訊息
+
+ */
